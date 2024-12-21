f064c1cbc3b5268722ed5596d7a4888cb35febf6
aman-maharshi <amanm4harshi@gmail.com>
2024-12-21 11:48:28 +0530

    expo init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        padding: 24
+      }}
+    >
+      <Text>Lorem ipsum dolor sit amet.</Text>
+      <Text>Lorem ipsum dolor sit amet.</Text>
+    </View>
+  );
+}
