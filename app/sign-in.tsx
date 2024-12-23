cbbb8cf651fcfb92e007f92971064530e8c1e73a
aman-maharshi <amanm4harshi@gmail.com>
2024-12-24 00:13:58 +0530

    login page ui updates

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,69 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import images from '@/constants/images'
+import icons from '@/constants/icons'
 
 const SignIn = () => {
+
+  const handleLogin = () => {
+    // Implement Google login
+  }
+
   return (
-    <View className='p-8'>
-      <Text>Sign In</Text>
-    </View>
+    <SafeAreaView className='h-full bg-white'>
+      <ScrollView contentContainerClassName='h-full'>
+        <Image
+          source={images.onboarding}
+          className='w-full h-4/6'
+          resizeMode='contain'
+        />
+
+        <View className='px-10'>
+          <Text className='text-center font-rubik uppercase text-black-200'>
+            Welcome to Restate
+          </Text>
+
+          <Text className='mt-2 text-3xl font-rubik-bold text-center text-black-300'>
+            Let's get you closer to {"\n"}
+            <Text className='text-primary-300'>Your ideal home</Text>
+          </Text>
+
+          <Text className='text-center mt-12 text-lg font-rubik text-black-200'>
+            Login to Restate with Google
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className='bg-white shadow-md shadow-zinc-300 border border-gray-100 rounded-full w-full py-4 mt-5'
+          >
+            <View className='flex-row justify-center items-center'>
+              <Image
+                source={icons.google}
+                className='h-5 w-5'
+                resizeMode='contain'
+              />
+              <Text className='text-lg font-rubik-medium text-black-300 ml-4'>
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default SignIn
+
+/*
+  NOTES:
+
+  ScrollView - to make the content scrollable
+  shadow-zinc-300 - better shoadow
+  <br> -> "\n"
+  button -> TouchableOpacity
+  image src -> source
+  resizeMode -> contain
+*/
