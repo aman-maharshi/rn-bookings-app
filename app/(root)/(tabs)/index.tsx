e39830580445c6f05b591bb2f22c1bedb50f7039
aman-maharshi <amanm4harshi@gmail.com>
2025-01-14 23:42:21 +0530

    filters component added

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import images from '@/constants/images'
 import icons from '@/constants/icons'
 import Search from "@/components/search"
 import { Card, FeaturedCard } from "@/components/cards"
+import Filters from "@/components/filters"
 
 const HomeScreen = () => {
   return (
@@ -49,8 +50,6 @@ const HomeScreen = () => {
             </View>
           </View>
 
-
-
           <View className="my-5">
             <View className="flex flex-row items-center justify-between">
               <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
@@ -58,6 +57,8 @@ const HomeScreen = () => {
                 <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
               </TouchableOpacity>
             </View>
+
+            <Filters />
 
             <View className="flex flex-row gap-5 mt-5">
               <Card />
